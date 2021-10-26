1410a91b13d57617271601117c93eb3919246c71
ramit batra <ramitbatra.official@gmail.com>
2021-10-26 17:09:25 +0530

    Finished project

--- proj.js
+++ proj.js
@@ -22,10 +22,12 @@ let configJSON = fs.readFileSync(args.config, "utf-8");
 let configJSO = JSON.parse(configJSON);
 console.log(configJSO);
 
+
 run();
 
 
 async function run() {
+  
   let browser = await puppeteer.launch({
     defaultViewport: null,
 
@@ -72,104 +74,111 @@ async function run() {
   await npage.keyboard.press("Tab");
   await npage.type("input[name='email']", tempmail);
     
-    await npage.keyboard.press('Tab');
-    await npage.keyboard.press('Tab');
-    await npage.keyboard.type('July');
+  await npage.keyboard.press('Tab');
+  await npage.keyboard.press('Tab');
+  await npage.keyboard.type('July');
 
-    await npage.keyboard.press('Tab');
-    await npage.keyboard.type('26');
+  await npage.keyboard.press('Tab');
+  await npage.keyboard.type('26');
 
-    await npage.keyboard.press('Tab');
-    await npage.keyboard.type('2001');
-    await npage.waitFor(2000);
+  await npage.keyboard.press('Tab');
+  await npage.keyboard.type('2001');
+  await npage.waitFor(2000);
 
-    await npage.keyboard.press('Tab');
-    await npage.waitFor(2000);
-    await npage.keyboard.press('Enter');
+  await npage.keyboard.press('Tab');
+  await npage.waitFor(2000);
+  await npage.keyboard.press('Enter');
 
-    await npage.waitFor(2000);
+  await npage.waitFor(2000);
 
-    await npage.keyboard.press('Tab');
-    await npage.waitFor(500);
-    await npage.keyboard.press('Tab');
-    await npage.waitFor(500);
-    await npage.keyboard.press('Tab');
-    await npage.waitFor(500);
-    await npage.keyboard.press('Tab');
-    await npage.waitFor(500);
-    await npage.keyboard.press('Enter');
+  await npage.keyboard.press('Tab');
+  await npage.waitFor(500);
+  await npage.keyboard.press('Tab');
+  await npage.waitFor(500);
+  await npage.keyboard.press('Tab');
+  await npage.waitFor(500);
+  await npage.keyboard.press('Tab');
+  await npage.waitFor(500);
+  await npage.keyboard.press('Enter');
     
-    for (let i = 0; i < 9;i++)
-    {
-        await npage.waitFor(500);
-    await npage.keyboard.press('Tab');
-    }
+  for (let i = 0; i < 9; i++) {
     await npage.waitFor(500);
-    await npage.keyboard.press('Enter');
+    await npage.keyboard.press('Tab');
+  }
+  await npage.waitFor(500);
+  await npage.keyboard.press('Enter');
 
-    let mpage = await browser.newPage();
-    await mpage.waitFor(10000);
-    await mpage.goto(configJSO.emailurl + tempmail);
+  let mpage = await browser.newPage();
+  await mpage.waitFor(10000);
+  await mpage.goto(configJSO.emailurl + tempmail);
 
-    await mpage.waitForSelector("td.h1.black");
-    let otp = await mpage.$eval("td.h1.black", function (h1tag) {
-        console.log("here");
-        return h1tag.textContent;
-      });
-      await mpage.close();
+  await mpage.waitForSelector("td.h1.black");
+  let otp = await mpage.$eval("td.h1.black", function (h1tag) {
+    console.log("here");
+    return h1tag.textContent;
+  });
+  await mpage.close();
       
-      let fotp =otp.trim();
-    console.log(fotp);
+  let fotp = otp.trim();
+  console.log(fotp);
     
 
-    await npage.waitFor(5000);
-    await npage.keyboard.type(fotp);
-    await npage.waitFor(500);
-    await npage.keyboard.press('Tab');
-    await npage.waitFor(500);
-    await npage.keyboard.press('Tab');
-    await npage.waitFor(500);
-    await npage.keyboard.press('Enter');
+  await npage.waitFor(5000);
+  await npage.keyboard.type(fotp);
+  await npage.waitFor(500);
+  await npage.keyboard.press('Tab');
+  await npage.waitFor(500);
+  await npage.keyboard.press('Tab');
+  await npage.waitFor(500);
+  await npage.keyboard.press('Enter');
 
-    await npage.waitFor(5000);
-    // await npage.keyboard.press('Tab');
-    await npage.keyboard.type(configJSO.password,{delay: 200 });
-    await npage.waitFor(1000);
-    await npage.keyboard.press('Tab',);
-    await npage.waitFor(1000);
-    await npage.keyboard.press('Tab');
-    await npage.waitFor(1000);
-    await npage.keyboard.press('Enter');
+  await npage.waitFor(5000);
+  // await npage.keyboard.press('Tab');
+  await npage.waitFor(1000);
+  await npage.keyboard.type(configJSO.password, { delay: 500 });
+  await npage.waitFor(1000);
+  await npage.keyboard.press('Tab',);
+  await npage.waitFor(1000);
+  await npage.keyboard.press('Tab');
+  await npage.waitFor(1000);
+  await npage.keyboard.press('Enter');
 
-    await npage.waitFor(1000);
-    // await npage.waitFor(500);
-    // await npage.keyboard.press('Tab',);
-    // await npage.waitFor(500);
-    // await npage.keyboard.press('Tab');
-    // await npage.waitFor(500);
-    // await npage.keyboard.press('Enter');
+  await npage.waitFor(1000);
+  // await npage.waitFor(500);
+  // await npage.keyboard.press('Tab',);
+  // await npage.waitFor(500);
+  // await npage.keyboard.press('Tab');
+  // await npage.waitFor(500);
+  // await npage.keyboard.press('Enter');
 
-    // await npage.waitFor(500);
-    // await npage.keyboard.press('Tab');
-    // await npage.waitFor(500);
-    // await npage.keyboard.press('Enter');
+  // await npage.waitFor(500);
+  // await npage.keyboard.press('Tab');
+  // await npage.waitFor(500);
+  // await npage.keyboard.press('Enter');
     
-    // await npage.waitFor(500);
-    // await npage.keyboard.press('Tab',);
-    // await npage.waitFor(500);
-    // await npage.keyboard.press('Tab');
-    // await npage.waitFor(500);
-    // await npage.keyboard.press('Enter');
-    await npage.waitFor(5000);
-    await npage.close();
-    let fpage = await browser.newPage();
-    await fpage.goto(configJSO.followurl);
-    await npage.waitFor(5000);
-    await fpage.waitForSelector("div[aria-label='Follow @sachin_rt']");
-    await npage.waitFor(5000);
-    await fpage.click("div[aria-label='Follow @sachin_rt']");
-    await npage.waitFor(5000);
-    await fpage.close();
+  // await npage.waitFor(500);
+  // await npage.keyboard.press('Tab',);
+  // await npage.waitFor(500);
+  // await npage.keyboard.press('Tab');
+  // await npage.waitFor(500);
+  // await npage.keyboard.press('Enter');
+  await npage.waitFor(5000);
+  await npage.close();
+
+  let urlarr = configJSO.followurl;
+
+  for (let i = 0; i < urlarr.length; i++) { 
+  let fpage = await browser.newPage();
+  await fpage.goto(urlarr[i]);
+  await npage.waitFor(5000);
+  await fpage.waitForSelector(".css-1dbjc4n.r-obd0qt.r-18u37iz.r-1w6e6rj.r-1h0z5md.r-dnmrzs");
+  await npage.waitFor(5000);
+  await fpage.click(".css-1dbjc4n.r-obd0qt.r-18u37iz.r-1w6e6rj.r-1h0z5md.r-dnmrzs");
+  await npage.waitFor(5000);
+
+  await fpage.close();
+}
     await page.close();
+  
 }
 
